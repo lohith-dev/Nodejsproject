a8f980b3af1c9e43fd692336e756ce90cc49b292
lohith-dev <srlohith92@gmail.com>
2023-10-19 19:11:18 +0530

    created server

--- Check.js
+++ Check.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const http = require('http');
+
+
+const server = http.createServer((req,res)=>{
+
+    console.log("Lohith");
+    res.write('<html>')
+    res.write('<head>Lohith</head>')
+    res.write('</html>')
+    res.end();
+})
+
+server.listen(4000);
