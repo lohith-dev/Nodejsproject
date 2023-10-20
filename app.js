9efce772cd59e857da9398b9b2321ab50c563ce1
lohith-dev <srlohith92@gmail.com>
2023-10-20 16:28:27 +0530

    body-parser

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ const routes = require('./routes')
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app= express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',(req,res,next)=>{
+    console.log('this alaways runs');
     next();
 })
 
-app.use((req,res,next)=>{
+app.use('/add-product',(req,res,next)=>{
+    console.log('In the middleware');
+    res.send('<body><form action="/product" method="POST"><input type="text" name ="title" placeholder="Enter the product"> <input type="text" name ="size" placeholder="Enter size"> <button type="submit">Send</button></body>')
+ 
+})
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+});
+
+app.use('/',(req,res,next)=>{
     console.log('In another middleware');
-    // res.send('<h1>hello to node js</h1>')
-    res.send( { key1: 'value' })
+    res.send('<h1>hello to node js</h1>')
 })
 
 
